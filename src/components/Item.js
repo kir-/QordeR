f9b47d5a4687d680579941a7288902b594a1f592
Grayson Lee <grayson@Graysons-MacBook-Air.local>
2019-08-29 12:18:42 -0700

    added packages

--- src/components/Item.js
+++ src/components/Item.js
@@ -3,10 +3,10 @@ import { makeStyles } from '@material-ui/core/styles';
 import Drawer from '@material-ui/core/Drawer';
 import Button from '@material-ui/core/Button';
 import List from '@material-ui/core/List';
-import Divider from '@material-ui/core/Divider';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
+// import Divider from '@material-ui/core/Divider';
+// import ListItem from '@material-ui/core/ListItem';
+// import ListItemIcon from '@material-ui/core/ListItemIcon';
+// import ListItemText from '@material-ui/core/ListItemText';
 import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
 import IconButton from '@material-ui/core/IconButton';
 
--- src/router.js
+++ src/router.js
@@ -2,10 +2,12 @@ import React from "react";
 import Restaurant from "./components/Restaurant";
 import Order from "./components/Order";
 import Application from "./components/Application";
+import Menu from "./components/Menu";
 const routes = {
   "/": () => <Application />,
   "/restaurant": () => <Restaurant />,
-  "/order": () => <Order />
+  "/order": () => <Order />,
+  "/menu": () => <Menu />
 };
 
 export default routes;
