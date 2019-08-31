3dbcda4e8e7fbe8afbf4a88e9dbe4dba26849f27
changbhc@gmail.com <changbhc@gmail.com>
2019-08-31 02:17:10 -0700

    partway through creating admin page

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -1,14 +1,52 @@
 import React, { Fragment } from 'react';
-import { FormControl, InputLabel, Input, FormHelperText } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles'
+import { TextField, FormControl, Button } from '@material-ui/core';
 import TopBar from './TopBar';
 
+const useStyles = makeStyles(theme => ({
+  textField: {
+    marginLeft: theme.spacing(1),
+    marginRight: theme.spacing(1),
+    width: 200,
+  },
+  button: {
+    margin: theme.spacing(1),
+  },
+}));
+
 export default function Admin() {
+  const classes = useStyles();
   return (
     <Fragment>
       <TopBar title="Admin Login for #restaurantName#"/>
       <br/>
       <br/>
       <br/>
+      <div class="row">
+        <div class="col offset-2">
+          <FormControl>
+            <TextField
+              id="standard-email-input"
+              label="Email"
+              className={classes.textField}
+              type="email"
+              autoComplete="current-email"
+              margin="normal"
+            />
+            <TextField
+              id="standard-password-input"
+              label="Password"
+              className={classes.textField}
+              type="password"
+              autoComplete="current-password"
+              margin="normal"
+            />
+          <Button type="submit" variant="outlined" color="primary" className={classes.button}>
+              Submit
+            </Button>
+          </FormControl>
+        </div>
+      </div>
     </Fragment>
   )
 }
