54b2911b1c67f8a844b0725d9c0fa63d166d7bcb
qkrwldnjsdl1 <vazywon@hotmail.com>
2019-08-30 22:04:18 -0700

    refactored menu.js

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -22,9 +22,135 @@ const useStyles = makeStyles(theme => ({
     paddingLeft: theme.spacing(4)
   }
 }));
-const orderList = {}
+const orderList = {};
+
+// fake datas
+const arr_app = [
+  "Miso Soup",
+  "House Green Salad",
+  "Gyoze",
+  "Edamame",
+  "Harumaki",
+  "Seaweed Salad",
+  "Agedashi Tofu",
+  "Sunomono Salad",
+  "Chicken Karage",
+  "Soft Shell Crab",
+  "Tuna Tataki",
+  "Potato Croquette",
+  "Tuna Carppachio"
+];
+const arr_tempura = [
+  "Prawn Tempura",
+  "Yam Tempura",
+  "Vegetable Tempura",
+  "Assorted Tempura",
+  "Appetizer Tempura",
+  "Sweet Potato Tempura"
+];
+const arr_udon = [
+  "Plain Udon",
+  "Beef Udon",
+  "Chicken Udon",
+  "Seafood Udon",
+  "Beef Yakiudon",
+  "Chicken Yakiudon",
+  "Seafood Yakiudon",
+  "Nabeyaki Udon"
+];
+const arr_carte = [
+  "Chicken Teriyaki",
+  "Beef Teriyaki",
+  "Chicken Curry",
+  "Beef Curry",
+  "Chicken Teriyaki Donburi",
+  "Beef Teriyaki Donburi",
+  "Sable Fish",
+  "Unagi Donburi"
+];
+
+const arr_maki = [
+  "Kappa Roll",
+  "Oshinko Roll",
+  "Salmon Roll",
+  "Tuna Roll",
+  "Negitoro Roll",
+  "California Roll",
+  "Salmon Avocado Roll",
+  "Yam Tempura Roll",
+  "BC Roll",
+  "Dynamite Roll",
+  "Mango Roll",
+  "Philadelphia Roll",
+  "Unagi Roll",
+  "Chopped Scallop Roll",
+  "Spicy Salmon Roll",
+  "Spicy Tuna Roll"
+];
+
+const arr_temaki = [
+  "Tuna Cone",
+  "Salmon Cone",
+  "Chopped Scallop Cone",
+  "Spicy Tuna Cone",
+  "Spicy Salmon Cone",
+  "Spicy Chopped Scallop Cone"
+];
+const arr_nigiri = [
+  "Inari",
+  "Tamago",
+  "Hokkigai",
+  "Wakame",
+  "Tuna",
+  "Salmon",
+  "Sockeye Salmon",
+  "Masago",
+  "Saba",
+  "Ebi",
+  "Chopped Scallop",
+  "Tobiko",
+  "Tai",
+  "Ika",
+  "Toro",
+  "Tobiko & Quall Egg",
+  "Smoke Salmon",
+  "Tako",
+  "Amaebi",
+  "Hotategai",
+  "Unagi",
+  "Ikura",
+  "Hamachi",
+  "Red Tuna",
+  "Uni"
+];
+
+const arr_sashimi = [
+  "Salmon Sashimi",
+  "Tuna Sashimi",
+  "Spicy Salmon Sashimi",
+  "Spicy Tuna Sashimi",
+  "Tuna & Salmon Sashimi",
+  "Sockeye Salmon Sashimi",
+  "Hokkigai Sashimi",
+  "Toro Sashimi",
+  "Tako Sashimi",
+  "Amaebi Sashimi",
+  "Hamachi Sashimi",
+  "Assorted Sashimi",
+  "Red Tuna Sashimi",
+  "Uni Sashimi"
+];
+
+const arr_combo = [
+  "Party Tray A",
+  "Party Tray B",
+  "Party Tray C",
+  "Spicy Combo"
+];
 
 export default function Menu() {
+  // Menu Components
+
   const classes = useStyles();
   const [appetizer, setAppetizer] = React.useState(false);
   const [tempura, setTempura] = React.useState(false);
@@ -35,163 +161,12 @@ export default function Menu() {
   const [nigiri, setNigiri] = React.useState(false);
   const [sashimi, setSashimi] = React.useState(false);
   const [combination, setCombination] = React.useState(false);
-
-
-  function handleAppetizer() {
-    setAppetizer(!appetizer);
-  }
-  function handleTempura() {
-    setTempura(!tempura);
-  }
-  function handleUdon() {
-    setUdon(!udon);
-  }
-  function handleCarte() {
-    setCarte(!carte);
-  }
-  function handleMaki() {
-    setMaki(!maki);
-  }
-  function handleTemaki() {
-    setTemaki(!temaki);
-  }
-  function handleNigiri() {
-    setNigiri(!nigiri);
-  }
-  function handleSashimi() {
-    setSashimi(!sashimi);
-  }
-  function handleCombination() {
-    setCombination(!combination);
-  }
-
-  const arr_app = [
-    "Miso Soup",
-    "House Green Salad",
-    "Gyoze",
-    "Edamame",
-    "Harumaki",
-    "Seaweed Salad",
-    "Agedashi Tofu",
-    "Sunomono Salad",
-    "Chicken Karage",
-    "Soft Shell Crab",
-    "Tuna Tataki",
-    "Potato Croquette",
-    "Tuna Carppachio"
-  ];
-  const arr_tempura = [
-    "Prawn Tempura",
-    "Yam Tempura",
-    "Vegetable Tempura",
-    "Assorted Tempura",
-    "Appetizer Tempura",
-    "Sweet Potato Tempura"
-  ];
-  const arr_udon = [
-    "Plain Udon",
-    "Beef Udon",
-    "Chicken Udon",
-    "Seafood Udon",
-    "Beef Yakiudon",
-    "Chicken Yakiudon",
-    "Seafood Yakiudon",
-    "Nabeyaki Udon"
-  ];
-  const arr_carte = [
-    "Chicken Teriyaki",
-    "Beef Teriyaki",
-    "Chicken Curry",
-    "Beef Curry",
-    "Chicken Teriyaki Donburi",
-    "Beef Teriyaki Donburi",
-    "Sable Fish",
-    "Unagi Donburi"
-  ];
-
-  const arr_maki = [
-    "Kappa Roll",
-    "Oshinko Roll",
-    "Salmon Roll",
-    "Tuna Roll",
-    "Negitoro Roll",
-    "California Roll",
-    "Salmon Avocado Roll",
-    "Yam Tempura Roll",
-    "BC Roll",
-    "Dynamite Roll",
-    "Mango Roll",
-    "Philadelphia Roll",
-    "Unagi Roll",
-    "Chopped Scallop Roll",
-    "Spicy Salmon Roll",
-    "Spicy Tuna Roll"
-  ];
-
-  const arr_temaki = [
-    "Tuna Cone",
-    "Salmon Cone",
-    "Chopped Scallop Cone",
-    "Spicy Tuna Cone",
-    "Spicy Salmon Cone",
-    "Spicy Chopped Scallop Cone"
-  ];
-  const arr_nigiri = [
-    "Inari",
-    "Tamago",
-    "Hokkigai",
-    "Wakame",
-    "Tuna",
-    "Salmon",
-    "Sockeye Salmon",
-    "Masago",
-    "Saba",
-    "Ebi",
-    "Chopped Scallop",
-    "Tobiko",
-    "Tai",
-    "Ika",
-    "Toro",
-    "Tobiko & Quall Egg",
-    "Smoke Salmon",
-    "Tako",
-    "Amaebi",
-    "Hotategai",
-    "Unagi",
-    "Ikura",
-    "Hamachi",
-    "Red Tuna",
-    "Uni"
-  ];
-
-  const arr_sashimi = [
-    "Salmon Sashimi",
-    "Tuna Sashimi",
-    "Spicy Salmon Sashimi",
-    "Spicy Tuna Sashimi",
-    "Tuna & Salmon Sashimi",
-    "Sockeye Salmon Sashimi",
-    "Hokkigai Sashimi",
-    "Toro Sashimi",
-    "Tako Sashimi",
-    "Amaebi Sashimi",
-    "Hamachi Sashimi",
-    "Assorted Sashimi",
-    "Red Tuna Sashimi",
-    "Uni Sashimi"
-  ];
-
-  const arr_combo = [
-    "Party Tray A",
-    "Party Tray B",
-    "Party Tray C",
-    "Spicy Combo"
-  ];
-  const [orderLength, setOrderLength] = React.useState(0)
-  const [cart, setCart] = React.useState(false)
+  const [orderLength, setOrderLength] = React.useState(0);
+  const [cart, setCart] = React.useState(false);
+  // These are states that need to be refactored later with Reducer or Redux
 
   if (!cart) {
-
+    // if Cart state is false it will render menu list
     return (
       <List
         component="nav"
@@ -203,7 +178,12 @@ export default function Menu() {
         }
         className={classes.root}
       >
-        <ListItem button onClick={handleAppetizer}>
+        <ListItem
+          button
+          onClick={() =>
+            appetizer ? setAppetizer(!appetizer) : setAppetizer(!appetizer)
+          }
+        >
           <ListItemIcon>
             <InboxIcon />
           </ListItemIcon>
@@ -212,10 +192,22 @@ export default function Menu() {
         </ListItem>
         <Collapse in={appetizer} timeout="auto" unmountOnExit>
           {arr_app.map(value => {
-            return <Item length={orderLength} setLength={setOrderLength} order={orderList} name={value} />;
+            return (
+              <Item
+                length={orderLength}
+                setLength={setOrderLength}
+                order={orderList}
+                name={value}
+              />
+            );
           })}
         </Collapse>
-        <ListItem button onClick={handleTempura}>
+        <ListItem
+          button
+          onClick={() =>
+            tempura ? setTempura(!tempura) : setTempura(!tempura)
+          }
+        >
           <ListItemIcon>
             <InboxIcon />
           </ListItemIcon>
@@ -225,11 +217,21 @@ export default function Menu() {
         <Collapse in={tempura} timeout="auto" unmountOnExit>
           <List component="div" disablePadding>
             {arr_tempura.map(value => {
-              return <Item length={orderLength} setLength={setOrderLength} order={orderList} name={value} />;
+              return (
+                <Item
+                  length={orderLength}
+                  setLength={setOrderLength}
+                  order={orderList}
+                  name={value}
+                />
+              );
             })}
           </List>
         </Collapse>
-        <ListItem button onClick={handleUdon}>
+        <ListItem
+          button
+          onClick={() => (udon ? setUdon(!udon) : setUdon(!udon))}
+        >
           <ListItemIcon>
             <InboxIcon />
           </ListItemIcon>
@@ -239,11 +241,21 @@ export default function Menu() {
         <Collapse in={udon} timeout="auto" unmountOnExit>
           <List component="div" disablePadding>
             {arr_udon.map(value => {
-              return <Item length={orderLength} setLength={setOrderLength} order={orderList} name={value} />;
+              return (
+                <Item
+                  length={orderLength}
+                  setLength={setOrderLength}
+                  order={orderList}
+                  name={value}
+                />
+              );
             })}
           </List>
         </Collapse>
-        <ListItem button onClick={handleCarte}>
+        <ListItem
+          button
+          onClick={() => (carte ? setCarte(!carte) : setCarte(!carte))}
+        >
           <ListItemIcon>
             <InboxIcon />
           </ListItemIcon>
@@ -253,11 +265,21 @@ export default function Menu() {
         <Collapse in={carte} timeout="auto" unmountOnExit>
           <List component="div" disablePadding>
             {arr_carte.map(value => {
-              return <Item length={orderLength} setLength={setOrderLength} order={orderList} name={value} />;
+              return (
+                <Item
+                  length={orderLength}
+                  setLength={setOrderLength}
+                  order={orderList}
+                  name={value}
+                />
+              );
             })}
           </List>
         </Collapse>
-        <ListItem button onClick={handleMaki}>
+        <ListItem
+          button
+          onClick={() => (maki ? setMaki(!maki) : setMaki(!maki))}
+        >
           <ListItemIcon>
             <InboxIcon />
           </ListItemIcon>
@@ -267,11 +289,21 @@ export default function Menu() {
         <Collapse in={maki} timeout="auto" unmountOnExit>
           <List component="div" disablePadding>
             {arr_maki.map(value => {
-              return <Item length={orderLength} setLength={setOrderLength} order={orderList} name={value} />;
+              return (
+                <Item
+                  length={orderLength}
+                  setLength={setOrderLength}
+                  order={orderList}
+                  name={value}
+                />
+              );
             })}
           </List>
         </Collapse>
-        <ListItem button onClick={handleTemaki}>
+        <ListItem
+          button
+          onClick={() => (temaki ? setTemaki(!temaki) : setTemaki(!temaki))}
+        >
           <ListItemIcon>
             <InboxIcon />
           </ListItemIcon>
@@ -281,11 +313,21 @@ export default function Menu() {
         <Collapse in={temaki} timeout="auto" unmountOnExit>
           <List component="div" disablePadding>
             {arr_temaki.map(value => {
-              return <Item length={orderLength} setLength={setOrderLength} order={orderList} name={value} />;
+              return (
+                <Item
+                  length={orderLength}
+                  setLength={setOrderLength}
+                  order={orderList}
+                  name={value}
+                />
+              );
             })}
           </List>
         </Collapse>
-        <ListItem button onClick={handleNigiri}>
+        <ListItem
+          button
+          onClick={() => (nigiri ? setNigiri(!nigiri) : setNigiri(!nigiri))}
+        >
           <ListItemIcon>
             <InboxIcon />
           </ListItemIcon>
@@ -295,11 +337,23 @@ export default function Menu() {
         <Collapse in={nigiri} timeout="auto" unmountOnExit>
           <List component="div" disablePadding>
             {arr_nigiri.map(value => {
-              return <Item length={orderLength} setLength={setOrderLength} order={orderList} name={value} />;
+              return (
+                <Item
+                  length={orderLength}
+                  setLength={setOrderLength}
+                  order={orderList}
+                  name={value}
+                />
+              );
             })}
           </List>
         </Collapse>
-        <ListItem button onClick={handleSashimi}>
+        <ListItem
+          button
+          onClick={() =>
+            sashimi ? setSashimi(!sashimi) : setSashimi(!sashimi)
+          }
+        >
           <ListItemIcon>
             <InboxIcon />
           </ListItemIcon>
@@ -309,11 +363,25 @@ export default function Menu() {
         <Collapse in={sashimi} timeout="auto" unmountOnExit>
           <List component="div" disablePadding>
             {arr_sashimi.map(value => {
-              return <Item length={orderLength} setLength={setOrderLength} order={orderList} name={value} />;
+              return (
+                <Item
+                  length={orderLength}
+                  setLength={setOrderLength}
+                  order={orderList}
+                  name={value}
+                />
+              );
             })}
           </List>
         </Collapse>
-        <ListItem button onClick={handleCombination}>
+        <ListItem
+          button
+          onClick={() =>
+            combination
+              ? setCombination(!combination)
+              : setCombination(!combination)
+          }
+        >
           <ListItemIcon>
             <InboxIcon />
           </ListItemIcon>
@@ -323,20 +391,27 @@ export default function Menu() {
         <Collapse in={combination} timeout="auto" unmountOnExit>
           <List component="div" disablePadding>
             {arr_combo.map(value => {
-              return <Item length={orderLength} setLength={setOrderLength} order={orderList} name={value} />;
+              return (
+                <Item
+                  length={orderLength}
+                  setLength={setOrderLength}
+                  order={orderList}
+                  name={value}
+                />
+              );
             })}
           </List>
         </Collapse>
-          <button onClick={()=> setCart(true)}>Checkout {orderLength}</button>
+        <button onClick={() => setCart(true)}>Checkout {orderLength}</button>
       </List>
     );
   } else {
-
+    // if cart state is true, it will render cart page
     return (
       <div>
-        <button onClick={()=> setCart(false)}>0==8</button>
-        <Cart order={orderList}/>
+        <button onClick={() => setCart(false)}>0==8</button>
+        <Cart order={orderList} />
       </div>
-    )
+    );
   }
 }
