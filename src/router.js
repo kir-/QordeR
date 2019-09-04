215a2f4314937abfa654ead71b9afd9e4016f7ea
changbhc@gmail.com <changbhc@gmail.com>
2019-09-04 10:19:32 -0700

    commiting before switching to work on Restaurant page: show tables. COME BACK AND WORK ON COOKIES FOR ADMIN LATER

--- src/router.js
+++ src/router.js
@@ -3,13 +3,14 @@ import Restaurant from "./components/Restaurant/index.js";
 import Order from "./components/Order";
 import Admin from "./components/Admin";
 import Application from "./components/Application";
+import { Cookies } from 'js-cookie';
 
 
 const routes = {
   "/": () => <Application />,
   "/restaurant/:id": ({ id }) => <Restaurant id={id}/>,
   "/:restoId/order/:id": ({ restoId, id }) => <Order restoId={restoId} id={id}/>,
-  "/admin": () => <Admin />,
+  "/admin": () => <Admin />
 };
 
 export default routes;
