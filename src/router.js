196bdc40c600bcf7da25616fc9d9d1f09b363e44
kir- <graysonclee@gmail.com>
2019-09-06 12:28:16 -0700

    Update router.js

--- src/router.js
+++ src/router.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Restaurant from "Restaurant/index.js";
+import Restaurant from "components/Restaurant/index";
 import Order from "components/Order";
 import Admin from "components/Admin";
 import Application from "components/Application";
